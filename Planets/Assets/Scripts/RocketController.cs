31e56156774148e20cfb0224e6e51fca5b2b4f27
Артемий Копейкин <Artemko.one@gmail.com>
2019-07-06 19:59:11 +0300

    Disabled OnMouseDown()

--- Planets/Assets/Scripts/RocketController.cs
+++ Planets/Assets/Scripts/RocketController.cs
@@ -21,11 +21,11 @@ public class RocketController : MonoBehaviour
     }
     void OnMouseDown()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var distance = Vector3.Distance(transform.position, mousePos);
-        float speedX = -(transform.position.x - mousePos.x) / distance;
-        float speedY = -(transform.position.y - mousePos.y) / distance;
-        gravity.AddSpeed(new Vector2(speedX, speedY));
+        //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //var distance = Vector3.Distance(transform.position, mousePos);
+        //float speedX = -(transform.position.x - mousePos.x) / distance;
+        //float speedY = -(transform.position.y - mousePos.y) / distance;
+        //gravity.AddSpeed(new Vector2(speedX, speedY));
         
     }
 }
