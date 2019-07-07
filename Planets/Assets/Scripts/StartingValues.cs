6e0539768a4e95073c850262b254db0c8b1a8eaf
Артемий Копейкин <Artemko.one@gmail.com>
2019-07-07 17:00:13 +0300

    New script StartingValues

---
+++ Planets/Assets/Scripts/StartingValues.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingValues : MonoBehaviour
+{
+    public Vector2 ForceVector;
+    public float ForceMagnitude;
+
+    public Rigidbody2D rb2d;
+
+    //private void Awake()
+    //{
+    //    rb2d = gameObject.GetComponent<Rigidbody2D>();
+    //}
+
+    private void Start()
+    {
+        rb2d.AddForce(ForceVector.normalized * ForceMagnitude);
+    }
+}
