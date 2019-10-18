add2b60c85b8607f479b07cefaf255c4e89d5441
sadSader <sadsader321@gmail.com>
2019-10-18 10:41:45 +0300

    Quick refactoring. Debug label

---
+++ Particles/Particles/Particles/src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Particles
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static Form1 window;
+        static public void Debug(string msg)
+        {
+            window.DebugMessage.Text = msg;
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            window = new Form1();
+            Application.Run(window);
+        }
+    }
+}
